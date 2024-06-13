f17c97ad70781eed44378f468bfe84ef308ad6b3
liandry24 <liandryursule@gmail.com>
2024-06-13 12:29:53 +0100

    week5 code

---
+++ code3/main.tf
@@ -0,0 +1,15 @@
+
+resource "aws_lightsail_instance" "custom" {
+  name              = "myserver"
+  availability_zone = "us-east-1b"
+  blueprint_id      = "amazon_linux_2"
+  bundle_id         = "nano_3_0"
+  user_data         = file("script.sh")
+key_pair_name = "week2"
+tags ={
+
+  Team ="Devops"
+  env ="dev"
+  create_by ="TERRAFORM"
+}
+}
